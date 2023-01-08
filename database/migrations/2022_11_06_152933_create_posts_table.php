a355572b62c1b11761fcd31191109fbd0ff6e3a8
Anochie Nwabueze <anochienwabueze@gmail.com>
2023-01-08 16:43:47 +0100

    Added migration files the application

---
+++ database/migrations/2022_11_06_152933_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('sub_title');
+            $table->longText('description');
+            $table->string('image');
+            $table->string('slug');
+            $table->unsignedBigInteger('category_id');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('view_count')->default(0);
+            $table->unsignedInteger('menu')->default(0);
+            $table->boolean('published')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
